9314969abf29d2e642f52cc273f738c9de4e9c86
FIRST_NAME LAST_NAME <xhuagf299@gmail.com>
2021-11-28 12:28:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/xirpl1_29_rakadasboarddesain/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.xirpl1_29_rakadasboarddesain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
